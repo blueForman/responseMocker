98bf2fbf95c08a1f4fe9e3e8c4d9cb808924623e
marko <marko.vusak2@gmail.com>
2020-01-28 18:20:13 +0100

    use doctrine to filter requests

--- src/Repository/ResponseRepository.php
+++ src/Repository/ResponseRepository.php
@@ -4,6 +4,12 @@
 namespace ResponseMocker\Repository;
 
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Criteria;
+use Doctrine\Common\Collections\ExpressionBuilder;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Query\Expr;
+use Doctrine\ORM\QueryBuilder;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
@@ -23,13 +29,17 @@ final class ResponseRepository
 
         $content = json_decode($serializedContent, true);
 
+        $responses = new ArrayCollection($content);
+
+        $eb = new ExpressionBuilder();
+        $expression = $eb->eq('queryString', $queryData);
+        $responseData = $responses->matching(new Criteria($expression))->first();
+
         //var_dump($content);
 
-        if (!isset($content[$queryData])) {
+        if (empty($responseData)) {
             throw new NotFoundHttpException();
         }
-
-        $responseData = $content[$queryData];
 
         return new JsonResponse($responseData['content'], $responseData['statusCode']);
     }
